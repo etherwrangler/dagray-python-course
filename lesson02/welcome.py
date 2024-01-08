d663d8840e575a7917930604abb7faa56db2dd84
etherwrangler <7719442+etherwrangler@users.noreply.github.com>
2024-01-07 21:25:32 -0600

    lesson 2

---
+++ lesson02/welcome.py
@@ -0,0 +1,11 @@
+line01 = "********************" # header / footer
+line02 = "*                  *" # re-use
+line03 = "*     WELCOME!     *"
+
+# starts with a blank line
+print('')
+print(line01) # prints the header / footer
+print(line02) # prints the second header / footer line
+print(line03) # prints the welcome message
+print(line02) # prints the second header / footer line
+print(line01) # prints the header footer
